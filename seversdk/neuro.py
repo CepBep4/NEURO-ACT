ef802fc88f548d303cb0fee702501e75b12eb171
Артём <sever@MacBook-Air-Artem-2.local>
2025-05-29 16:28:29 +0300

    0.1.1

--- seversdk/neuro.py
+++ seversdk/neuro.py
@@ -16,7 +16,7 @@ def pipe(model, tokenizer, prompt) -> str | dict:
     inputs = tokenizer(prompt, return_tensors="pt").to(model.device)
 
     with torch.no_grad():
-        outputs = model.generate(**inputs, max_new_tokens=1000)
+        outputs = model.generate(**inputs, max_new_tokens=200)
 
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
     
